2111f81384c2cc8f4fb8d9fe8fba262ed089de50
Bananabit <bananabit@web.de>
2025-08-24 14:14:15 +0200

    added asset_dir in dioxus.toml

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,6 @@ fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        script { src: "https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"},
         Router::<Route> {}
     }
 }
