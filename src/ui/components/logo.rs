402981e5f51a82f3fe23e3bb48caee6d780fc9ba
Bananabit <bananabit@web.de>
2025-08-24 23:58:05 +0200

    fixed asset macro now using src and href instead

--- src/ui/components/logo.rs
+++ src/ui/components/logo.rs
@@ -1,10 +1,12 @@
 use dioxus::prelude::*;
+/*
+const _HEADER_SVG: Asset = asset!(); 
+*/
 
-const _HEADER_SVG: Asset = asset!("/assets/header.svg");
 
 #[component]
 pub fn Logo() -> Element {
     rsx! {
-            img { src: _HEADER_SVG, id: "header" }
+            img { src: "/assets/header.svg", id: "header" }
     }
 }
